6ec885377f271f6d1ec905c9ac14a6828621d3c9
Giri Prasad P <giriprasa@gmail.com>
2024-08-06 19:36:32 +0530

    Sample 1234 updated

--- Sample2.java
+++ Sample2.java
@@ -1,5 +1,5 @@
 class Sample2{
-    public public static void main(String[] args) {
+     public static void main(String[] args) {
       System.out.println("Hello");   
     }
 }
--- Sample4.java
+++ Sample4.java
@@ -1,5 +1,5 @@
 class Sample4{
-    public public static void main(String[] args) {
+     public static void main(String[] args) {
       System.out.println("Hello");   
     }
 }
